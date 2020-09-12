bf196c738d20815442b9e27273c4bbb8c81300ec
abduallah <abdullah.mansour97@gmail.com>
2020-09-12 16:56:47 +0200

    first commit

---
+++ android/app/src/main/kotlin/flutter/ss/softagi/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package flutter.ss.softagi.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                    ),
+                  ),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Icon(
+                    Icons.ac_unit,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                'Login with your data that you register by it.',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Text(
+                'You Email',
+                style: TextStyle(
+                  fontSize: 15.0,
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.0,
+                    color: Colors.grey,
+                  ),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'name@domain.com',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Password',
+                style: TextStyle(
+                  fontSize: 15.0,
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.0,
+                    color: Colors.grey,
+                  ),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                child: TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'at least 8 characters',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Checkbox(
+                    onChanged: (bool value) {},
+                    value: true,
+                  ),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  Text(
+                    'Keep me logged in',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.indigo,
+                ),
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Don\'t have an account?'),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
